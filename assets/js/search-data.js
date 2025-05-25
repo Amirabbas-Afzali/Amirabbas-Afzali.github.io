dbd6580a424a1712fee83bbbfd540aa863a49861
Amirabbas-Afzali <102149705+Amirabbas-Afzali@users.noreply.github.com>
2025-05-25 08:31:21 +0000

    Deploying to gh-pages from @ Amirabbas-Afzali/Amirabbas-Afzali.github.io@d10998edd75a75d3e3f7c2670909156a5742f487 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,13 +447,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/amir-a-afzali-b539b1226", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
